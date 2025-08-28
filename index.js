5f49cd0fdd463f5b5cf9ebf8da5f2b3445d46f93
Mitu-Rani-Sutradhar <mitusutradhar4965@gmail.com>
2025-08-28 22:41:02 +0600

    added heart button event listener on js file

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // console.log("hello")
+
+function getElement(id) {
+  const element = document.getElementById(id);
+  return element;
+}
+
+const cardHeartBtns =document.getElementsByClassName("card-heart-btns");
+// console.log(cardHeartBtns);
+
+for(let cardHeartBtn of cardHeartBtns){
+    cardHeartBtn.addEventListener("click",function(){
+    
+        const totalHeart = getElement("total-heart").innerText;
+        const currentTotal = Number(totalHeart) + 1;
+        getElement("total-heart").innerText = currentTotal;
+
+    });
+
+}
